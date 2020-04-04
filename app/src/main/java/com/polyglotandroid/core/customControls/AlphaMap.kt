39514a3762a5a6e91bff957bf626a3ff092eedce
Huy-Ngo <duchuy29092000@gmail.com>
2020-04-04 19:43:43 +0700

    Finalize AlphaMap

--- app/src/main/java/com/polyglotandroid/core/customControls/AlphaMap.kt
+++ app/src/main/java/com/polyglotandroid/core/customControls/AlphaMap.kt
@@ -1,10 +1,12 @@
 package com.polyglotandroid.core.customControls
 
+import java.util.*
+import kotlin.collections.HashMap
+
 /**
  * Provides alphabetical ordering map-compatible with multi-unicode character,
  * constructed lettering.
  */
-/
 class AlphaMap<K, V> {
     private var longestEntry: Int = 0
     private val delegate: HashMap<K, V> = hashMapOf()
@@ -18,9 +20,15 @@ class AlphaMap<K, V> {
             longestEntry = keyLen
         return delegate.put(key, orderValue)
     }
-    // TODO: containsKey
-    // TODO: get(K key)
-    // TODO: clear()
-    // TODO: equals()
-    // TODO: hashCode()
+    fun containsKey(key: K): Boolean = delegate.containsKey(key)
+    fun get(key: K): V? = delegate.get(key)
+    fun clear() = delegate.clear()
+    override fun equals(comp: Any?): Boolean {
+        var ret = false
+        if (comp is AlphaMap<*, *>)
+            ret = (comp.longestEntry == this.longestEntry && comp.delegate == this.delegate)
+        return ret
+    }
+
+    // NOTE: Objects.hashCode() requires higher level API.
 }
