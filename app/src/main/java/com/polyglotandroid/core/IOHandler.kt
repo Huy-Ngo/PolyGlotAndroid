ed0b86d2a40ec723380a37b6676c292b3e8e010d
Huy-Ngo <duchuy29092000@gmail.com>
2020-05-03 20:05:07 +0700

    Add ReversionNode

--- app/src/main/java/com/polyglotandroid/core/IOHandler.kt
+++ app/src/main/java/com/polyglotandroid/core/IOHandler.kt
@@ -1,6 +1,9 @@
 package com.polyglotandroid.core
 
+import org.xml.sax.SAXException
 import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class IOHandler {
     companion object {
@@ -10,6 +13,55 @@ class IOHandler {
         fun getFilenameFromPath(fullPath: String): String? {
             val file = File(fullPath)
             return file.name
+        }
+
+        fun writeErrorLog(exception: SAXException, comment: String = "") {
+            var curContents = ""
+            var errorMessage: String =
+                DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now())
+            errorMessage += "-" + exception.localizedMessage
+                .toString() + "-" + exception.getClass().getName()
+            var rootCause: Throwable = ExceptionUtils.getRootCause(exception)
+            rootCause = rootCause ?: exception
+            errorMessage += """
+                
+                ${ExceptionUtils.getStackTrace(rootCause)}
+                """.trimIndent()
+
+            if (!comment.isEmpty()) {
+                errorMessage = "$comment:\n$errorMessage"
+            }
+
+            val errorLog = File(
+                PGUtil.getErrorDirectory().getAbsolutePath()
+                    .toString() + File.separator + PGUtil.ERROR_LOG_FILE
+            )
+
+            try {
+                if (errorLog.exists()) {
+                    Scanner(errorLog).useDelimiter("\\Z").use({ logScanner ->
+                        curContents = if (logScanner.hasNext()) logScanner.next() else ""
+                        val length = curContents.length
+                        val newLength = length + errorMessage.length
+                        if (newLength > PGUtil.MAX_LOG_CHARACTERS) {
+                            curContents =
+                                curContents.substring(newLength - PGUtil.MAX_LOG_CHARACTERS)
+                        }
+                    })
+                }
+                BufferedWriter(FileWriter(errorLog)).use({ writer ->
+                    val output: String =
+                        getSystemInformation().toString() + "\n" + curContents + errorMessage + "\n"
+                    println("Writing error to: " + errorLog.absolutePath)
+                    writer.write(output)
+                })
+            } catch (e: IOException) {
+                // Fail silently. This fails almost exclusively due to being run in write protected folder, caught elsewhere
+                // do not log to written file for obvious reasons (causes further write failure)
+                // WHY DO PEOPLE INSTALL THIS TO WRITE PROTECTED FOLDERS AND SYSTEM32. WHY.
+                // IOHandler.writeErrorLog(e);
+            }
+
         }
     }
 }
---
+++ app/src/main/java/com/polyglotandroid/core/nodes/ReversionNode.kt
@@ -0,0 +1,93 @@
+package com.polyglotandroid.core.nodes
+
+import com.polyglotandroid.core.IOHandler
+import com.polyglotandroid.core.PGUtil
+import org.w3c.dom.Document
+import org.w3c.dom.Node
+import org.xml.sax.SAXException
+import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.io.InputStream
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.parsers.ParserConfigurationException
+
+
+/**
+ * A node representing one prior state of a language XML file
+ */
+class ReversionNode : Comparable<ReversionNode?> {
+    val value: ByteArray
+    private var saveTime: Instant
+
+    constructor(_value: ByteArray) {
+        value = _value
+        saveTime = Instant.MIN
+        populateTimeFromDoc()
+    }
+
+    constructor(_value: ByteArray, _saveTime: Instant) {
+        value = _value
+        saveTime = _saveTime
+    }
+
+    /**
+     * Isolates lengthy process in individual thread
+     */
+    private fun populateTimeFromDoc() {
+        object : Thread() {
+            override fun run() {
+                saveTime = lastSaveTimeFromRawDoc
+            }
+        }.start()
+    }
+
+    private val lastSaveTimeFromRawDoc: Instant
+        private get() {
+            var ret: Instant
+            try {
+                val `is`: InputStream = ByteArrayInputStream(value)
+                val dbFactory: DocumentBuilderFactory = DocumentBuilderFactory.newInstance()
+                val dBuilder: DocumentBuilder = dbFactory.newDocumentBuilder()
+                val doc: Document
+                doc = dBuilder.parse(`is`)
+                doc.getDocumentElement().normalize()
+                val timeNode: Node = doc.getElementsByTagName(PGUtil.DICTIONARY_SAVE_DATE).item(0)
+                ret = if (timeNode != null) {
+                    Instant.parse(timeNode.getTextContent())
+                } else {
+                    Instant.MIN
+                }
+            } catch (e: SAXException) {
+                IOHandler.writeErrorLog(e)
+                ret = Instant.MIN
+            } catch (e: IOException) {
+                IOHandler.writeErrorLog(e)
+                ret = Instant.MIN
+            } catch (e: ParserConfigurationException) {
+                IOHandler.writeErrorLog(e)
+                ret = Instant.MIN
+            }
+            return ret
+        }
+
+    override fun toString(): String {
+        var ret = "saved: "
+        if (!saveTime.equals(Instant.MIN)) {
+            val formatter: DateTimeFormatter =
+                DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss").withZone(ZoneId.systemDefault())
+            ret += formatter.format(saveTime)
+        } else {
+            ret += "<UNKNOWN TIME>"
+        }
+        return ret
+    }
+
+    override fun compareTo(other: ReversionNode?): Int {
+        // returns in reverse order
+        return -saveTime.compareTo(other.saveTime)
+    }
+}
